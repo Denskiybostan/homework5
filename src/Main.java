b14854519c393a8fb10e4f4fd00a56571af2be8c
denis <deniz246@mail.ru>
2023-10-25 20:11:05 +0300

    задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.util.Arrays;
 
 public class Main {
-    private static double aDouble;
-
     public static void main(String[] args) {
         int [] number = new int[3];
         number[0] = 1;
